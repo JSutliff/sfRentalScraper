1931e11ce2d1bf714d64af7e27ee06a9177c786e
Jason Sutliff <jbopsut@gmail.com>
2018-10-15 10:05:04 -0700

    typo fixed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 // ====================================================
 
 mongoose.Promise = Promise;
-var MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/sfRentalScrapper"
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/sfRentalScrapper"
 
 mongoose.connect(MONGODB_URI);
 
